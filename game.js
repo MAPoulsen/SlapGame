72672dd509d1744d667b81dbf4500bb8b4ede1f2
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-04-21 22:46:33 -0600

    sprite

--- game.js
+++ game.js
@@ -1,21 +1,125 @@
-var health= 100;
 
-function slap(){
-    var health= 100;
-    health =+ 1;
-    alert ('health')
+var hanzHealth = 100;
+var franzHealth = 100;
+
+function hanzResponses(){
+  var choice = ['duck', 'block', 'dayDream', "takeHit",];
+  var compChoice = choice[Math.floor(Math.random() * choice.length)];
+
+  if(compChoice === "duck"){
+      hanzHealth += 10;
+      alert("He avoids his opponent by ducking!");
+    }
+    else if(compChoice === "block"){
+      hanzHealth += 5;
+      alert("He avoids the hit with a block!");
+    }
+  update()
+    }
+
+  hanzResponses()
+
+  function franzResponses(){
+  var choice = ['duck', 'block', 'dayDream', "takeHit",];
+  var compChoice = choice[Math.floor(Math.random() * choice.length)];
+
+  if(compChoice === "duck"){
+      franzHealth += 10;
+      alert("He avoids his opponent by ducking!");
+    }
+    else if(compChoice === "block"){
+      franzHealth += 5;
+     alert("He avoids the hit with a block!");
+    }
+    update()
+  }
+  franzResponses()
+
+
+
+
+function franzPunch() {
+    hanzResponses();
+    hanzHealth -= 10;
+    if (hanzHealth <= 0) {
+        alert("Franz wins!");
+        reset();
+    }
+    update()
 }
-slap()
+function hanzPunch() {
+    franzResponses();
+    franzHealth -= 10;
+    if (franzHealth <= 0) {
+        alert("Hanz wins!!!");
+        reset();
+    }
+    update()
+}
+function franzUpper() {
+    hanzResponses();
+    hanzHealth -= 20;
+    if (hanzHealth <= 0) {
+        alert("Franz wins!");
+        reset();
+    }
+    update()
+}
+function hanzUpper() {
+    franzResponses();
+    franzHealth -= 20;
+    if (franzHealth <= 0) {
+        alert("Hanz wins!!!");
+        reset();
+    }
+    update()
+}
+
+function reset() {
+    hanzHealth = 100;
+    franzHealth = 100;
+    update()
+}
+
+function update() {
+    var hanzHealthBarElem = document.getElementById('hanz-health-bar');
+    var franzHealthBarElem = document.getElementById('franz-health-bar');
+
+    hanzHealthBarElem.style.width = hanzHealth + '%';
+    franzHealthBarElem.style.width = franzHealth + '%'
+}
+
+// working code
+
+
+// var player = {
+//     health: 100,
+//     hits: 0,
+//     items:[items.shield],
+//     addMods: function(params) {
+//         var total=0;
+//         for(var i=0;i< this.items.length;i++) {
+//             total = total + this.items[i].modifier;
+//         }  
+//         return total; 
+//     }
+// }
 
 
 
+// function jab(name) {
+//     if (player.name === "Hanz") {
+//         updatePlayerHealth(name);
+//         health -= 10;
+//     }
+// }
+// jab()
 
-
-
-
-
-
-
+//  function slap(){
+//      player.health = player.health - (10 - (1 * player.addMods())); 
+//      player.hits = player.hits +1;
+//      upDate();
+//      }
 
 
 
@@ -32,19 +136,7 @@ slap()
 //     sword:new Item("sword", 0.5, "This is an awesome sword!"),
 //     potion:new Item("potion", 0.7,"This is an awesome potion!"),
 // }
-// var player = {
-//     health: 100,
-//     hits: 0,
-//     items:[items.shield],
-//     addMods: function(params) {
-//         var total=0;
-//         for(var i=0;i< this.items.length;i++) {
-//             total = total + this.items[i].modifier;
-//         }  
-//         return total; 
-//     }
-// }
- 
+
 //  var player = {
 //     health: 100,
 //     hits: 0,
@@ -57,7 +149,7 @@ slap()
 //         return total; 
 //     }
 // }
- 
+
 //   var player = {
 //     health: 100,
 //     hits: 0,
@@ -70,7 +162,7 @@ slap()
 //         return total; 
 //     }
 // }
- 
+
 //   var player = {
 //     health: 100,
 //     hits: 0,
@@ -84,25 +176,25 @@ slap()
 //     }
 // }
 // //  Adding modds for weapons above
- 
+
 //  function slap(){
 //      player.health = player.health - (10 - (1 * player.addMods())); 
 //      player.hits = player.hits +1;
 //      upDate();
 //      }
-  
+
 //  function punch(){
 //      player.health = player.health - (10 - (5 * player.addMods())); 
 //      player.hits = player.hits +1;
 //      upDate();
 //      }
-     
+
 //  function kick(){
 //      player.health = player.health - (10 - (10 * player.addMods())); 
 //      player.hits = player.hits +1;
 //      upDate();    
 //      }
- 
+
 // // update function
 //  function upDate(){
 //      var playerHealth = document.getElementById("player-health");
@@ -129,9 +221,9 @@ slap()
 //      document.getElemById("player-items")
 //     }
 // }
- 
+
 //  upDate();
 
 
-  
+
 
