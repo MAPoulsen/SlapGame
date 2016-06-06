db7316919082ce26a3144106beb46594991cf978
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-06 11:48:33 -0600

    mjb

--- game.js
+++ game.js
@@ -117,7 +117,7 @@ function hanzAnimateUpper() {
 
 function franzAnimatePunch() {
     $('#Franz').empty();
-    $('#Franz').append('<img src="img/fPunch.gif">')
+    $('#Franz').append('<img src="img/fPunch.gif" style="height: 90%">')
     setTimeout(function () {
         $('#Franz').empty();
         $('#Franz').append('<img src="img/franz.png">')
