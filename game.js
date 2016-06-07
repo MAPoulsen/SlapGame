6cb275bbcdce742adf4a4fad1a82541847979ce8
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-07 10:13:22 -0600

    h

--- game.js
+++ game.js
@@ -53,12 +53,10 @@ function hanzResponses() {
     if (compChoice === "duck") {
         hanzAnimateDuck();
         hanzHealth += 10;
-        //alert("He avoids his opponent by ducking!");
     }
     else if (compChoice === "block") {
         hanzAnimateBlock();
         hanzHealth += 5;
-        //alert("He avoids the hit with a block!");
     }
     update()
 }
@@ -218,153 +216,5 @@ function hanzUnder() {
     }
      }
 }
-
-
-// healthbar
-
-// working code
-
-
-// var player = {
-//     health: 100,
-//     hits: 0,
-//     items:[items.shield],
-//     addMods: function(params) {
-//         var total=0;
-//         for(var i=0;i< this.items.length;i++) {
-//             total = total + this.items[i].modifier;
-//         }  
-//         return total; 
-//     }
-// }
-
-
-
-// function jab(name) {
-//     if (player.name === "Hanz") {
-//         updatePlayerHealth(name);
-//         health -= 10;
-//     }
-// }
-// jab()
-
-//  function slap(){
-//      player.health = player.health - (10 - (1 * player.addMods())); 
-//      player.hits = player.hits +1;
-//      upDate();
-//      }
-
-
-
-
-
-
-
-
-
-
-// var items = {
-//     shield:new Item("shield",0.3,"This is an awesome shield!"),
-//     armor:new Item("armor",0.4,"This is awesome armor!"),
-//     sword:new Item("sword", 0.5, "This is an awesome sword!"),
-//     potion:new Item("potion", 0.7,"This is an awesome potion!"),
-// }
-
-//  var Hanz = {
-//         name: "Hanz",
-//         health: 100,
-//     img: src="img/Hanz.png",
-//     items:[items.armor],
-//     addMods: function(params) {
-//         var total=0;
-//         for(var i=0;i< this.items.length;i++) {
-//             total = total + this.items[i].modifier;
-//         }  
-//         return total; 
-//     }
-// }
-
-//   var player = {
-//     health: 100,
-//     hits: 0,
-//     items:[items.sword],
-//     addMods: function(params) {
-//         var total=0;
-//         for(var i=0;i< this.items.length;i++) {
-//             total = total + this.items[i].modifier;
-//         }  
-//         return total; 
-//     }
-// }
-
-//   var player = {
-//     health: 100,
-//     hits: 0,
-//     items:[items.potion],
-//     addMods: function(params) {
-//         var total=0;
-//         for(var i=0;i< this.items.length;i++) {
-//             total = total + this.items[i].modifier;
-//         }  
-//         return total; 
-//     }
-// }
-// //  Adding modds for weapons above
-
-//  function slap(){
-//      player.health = player.health - (10 - (1 * player.addMods())); 
-//      player.hits = player.hits +1;
-//      upDate();
-//      }
-
-//  function punch(){
-//      player.health = player.health - (10 - (5 * player.addMods())); 
-//      player.hits = player.hits +1;
-//      upDate();
-//      }
-
-//  function kick(){
-//      player.health = player.health - (10 - (10 * player.addMods())); 
-//      player.hits = player.hits +1;
-//      upDate();    
-//      }
-
-// // update function
-//  function upDate(){
-//      var playerHealth = document.getElementById("player-health");
-//      playerHealth.innerText = player.health.toString();
-//      var hitsIncrease = document.getElementById("playerHits");
-//      hitsIncrease.innerText = player.hits.toString();
-//      if(player.health <= 10){
-//          document.getElementById("player-panel").classList.add("panel-danger")
-//     }else{
-//          document.getElementById("player-panel").classList.remove("panel-danger")
-//     }
-//      if(player.health === 0){
-//        //alert: "You are dead!";    
-//     }
-//  }
-// // update functions above
-// // new objects to 
-
-// function Item (name, modifier, description) {
-//     this.name = name;
-//     this.modifier = modifier;
-//     this.description = description;
-//     this.draw = function(){
-//      document.getElemById("player-items")
-//     }
-// }
-
-//  upDate();
-
-// function attack (name, modifier, description){
-//     this.name = name;
-//     this.modifier = modifier;
-//     this.description = description;
-//     this.draw= function(){
-//        document.getElemById('player-items') 
-//     }
-// }
 
 
